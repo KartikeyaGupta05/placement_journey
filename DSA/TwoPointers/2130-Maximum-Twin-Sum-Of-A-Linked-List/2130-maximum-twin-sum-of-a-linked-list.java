edd6614634b31729cd250982469cf9e41e0cc029
Kartikeya Gupta <151367534+KartikeyaGupta05@users.noreply.github.com>
2026-06-24 22:23:21 +0530

    Add solution for maximum twin sum of a linked list
    Implement the solution to find the maximum twin sum of a linked list.

---
+++ DSA/TwoPointers/2130-Maximum-Twin-Sum-Of-A-Linked-List/2130-maximum-twin-sum-of-a-linked-list.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int pairSum(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode prev = null, curr = slow, next = null;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        int ans = 0;
+        ListNode first = head, second = prev;
+        while (second != null) {
+            ans = Math.max(ans, first.val + second.val);
+            first = first.next;
+            second = second.next;
+        }
+
+        return ans;
+    }
+}
